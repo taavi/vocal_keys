97bd431f424cf360eb59f0590233504ef4832904
Taavi Burns <taavi@taaviburns.ca>
2015-05-14 09:51:31 -0400

    First crack at the actual glue.

---
+++ lighting/glue.py
@@ -0,0 +1,48 @@
+import time
+
+import rtmidi
+import fake7221
+
+spi = fake7221.Fake7221()
+
+class LinearDisplay(object):
+    BIT_TRANSLATOR = {
+        x: x
+        for x in range(8)
+    }
+
+    def __init__(self, spi_7221):
+        self._spi_7221 = spi_7221
+        self._state = [0] * 4
+
+    def __setitem__(self, key, do_set):
+        if key >= (len(self._state) * 8):
+            return
+        digit, bit = divmod(key, 8)
+        mask = 1 << BIT_TRANSLATOR[bit]
+        if do_set:
+            self._state[digit] |= mask
+        else:
+            self._state[digit] &= ~(mask)
+        self._spi_7221.write(nums2str(DIG0 + digit, self._state[digit]))
+
+
+display = LinearDisplay(spi)
+
+KEYDOWN = 144
+KEYUP = 128
+MIDI_C2 = 48
+MIDI_C4 = 72
+
+def callback(event, data):
+    ((event_type, note, velocity), time_delta) = event
+    display[note - MIDI_C2] = True if event_type == KEYDOWN else False
+    print spi
+
+
+midi = rtmidi.MidiIn()
+midi.open_port(name="Some Name")
+midi.set_callback(callback)
+
+while True:
+    sleep(1)

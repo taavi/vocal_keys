d6865d31d6284cfec0184f4dea328f8a7c1a25e3
Taavi Burns <taavi@taaviburns.ca>
2015-05-25 01:19:08 -0400

    Fixes bit ordering for lighting

--- lighting/glue.py
+++ lighting/glue.py
@@ -36,8 +36,14 @@ setup_thread.start()
 
 class LinearDisplay(object):
     BIT_TRANSLATOR = {
-        x: x
-        for x in range(8)
+        0: 6,
+        1: 5,
+        2: 4,
+        3: 3,
+        4: 2,
+        5: 1,
+        6: 0,
+        7: 7,
     }
 
     def __init__(self, queue):
